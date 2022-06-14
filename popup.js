0eff739f0c685f22605f9cd463829dd730479204
Aman Shinde <amanshindehulk@gmail.com>
2022-06-14 23:41:29 +0530

    feat(delete) delete operation is added

--- popup.js
+++ popup.js
@@ -2,12 +2,12 @@ document.getElementById("toggle").addEventListener("click", toggleMode);
 document.getElementById("new_note_button").addEventListener("click", getNewNoteData);
 
 function toggleMode() {
-    var button_class = document.getElementById('main').className;
-    if (button_class.includes('dark-mode')) {
+    var buttonClass = document.getElementById('main').className;
+    if (buttonClass.includes('dark-mode')) {
         document.getElementById('main').className = 'main-container';
     }
     else {
-        document.getElementById('main').className = button_class + " dark-mode";
+        document.getElementById('main').className = buttonClass + " dark-mode";
     }
 }
 
@@ -15,64 +15,38 @@ const notesContainer = document.querySelector('.notes-container');
 
 //main function to each and every function 
 function main() {
-
     if (!window.localStorage.getItem('note')) {
-        window.localStorage.setItem('note', JSON.stringify({ "data_cn": [{id:'1',timeStamp:'',text:'Hi'},] }))
+        window.localStorage.setItem('note', JSON.stringify({ "data_cn": [{ id: '1', timeStamp: '', text: 'Hi' },] }))
     }
 
-    updateUi();
-}
-
-//Takes note as an argument and add necessary code on frontend
-function displayNote(note, newNote = false) {
-    let newNoteHTML = `            
-    <div class='note'>
-        <span> ${note}</span>
-        <div class="note-footer">
-            <button class='button delete'>Delete</button>
-        </div>
-    </div>`;
-    if (newNote) {
-        notesContainer.innerHTML = newNoteHTML + notesContainer.innerHTML;
-    }
-    else {
-        notesContainer.innerHTML = notesContainer.innerHTML + newNoteHTML;
-    }
-};
-
-//Retrive notes from localstorage and call displaynote function with note-text as an arggument
-function updateUi(newNote = false) {
-
-    var notes = getAllNotes();
-    if (newNote) {
-        displayNote(notes.data_cn[0].text, true);
-    }
-    else {
-        notes.data_cn.forEach(note => {
-            console.log(note.text);
-            displayNote(note.text);
-        });
-    }
+    loadUi();
 }
 
 //Takes data as an argument and add it to the localstorage
 function addNewNote(new_note_text) {
     var notes = getAllNotes();
-    var new_note = {id:uniqueId(),timeStamp:'',text:new_note_text}
-    console.log("Before:    ", notes.data_cn);
-    window.localStorage.setItem('note', JSON.stringify({ "data_cn": [new_note, ...notes.data_cn] }))
-    updateUi(true);
+    var new_note = { id: uniqueId(), timeStamp: '', text: new_note_text }
+    localStorage.setItem('note', JSON.stringify({ "data_cn": [new_note, ...notes.data_cn] }))
+    loadUi();
 }
 
 function deleteNote() {
-
+    var notesList = getAllNotes();
+    var newNotesList = notesList.data_cn.filter(noteItem => noteItem.id != this.id);
+    if(newNotesList)
+    {
+        localStorage.setItem('note', JSON.stringify({ "data_cn": [...newNotesList] }));
+    }
+    else
+    {
+        localStorage.setItem('note', JSON.stringify({ "data_cn": [] }));
+    }
+    loadUi();
 }
 
 //gets input value on button click
 function getNewNoteData() {
-
     var new_note_text = document.getElementById("note_text").value;
-    console.log("Clicked ", new_note_text);
     document.getElementById("note_text").value = '';
     addNewNote(new_note_text);
 }
@@ -92,10 +66,34 @@ function uniqueId() {
         date.getSeconds(),
         date.getMilliseconds()
     ];
-
     var id = components.join("");
-
     return id;
+}
+
+function loadUi() {
+    var noteList = getAllNotes();
+    if (noteList.data_cn.length > 0 ) {
+        notesContainer.innerHTML = '';
+        noteList.data_cn.forEach((noteItem) => {
+            var newNoteHTML = `            
+            <div class='note' >
+                <span> ${noteItem.text}</span>
+                <div class="note-footer">
+                    <button class='button delete' id='${noteItem.id}'>Delete</button>
+                </div>
+            </div>`;
+
+            notesContainer.innerHTML =  notesContainer.innerHTML + newNoteHTML ;
+        })
+
+        notesContainer.querySelectorAll(".delete").forEach(noteListItem => {
+            noteListItem.addEventListener("click", deleteNote)
+        })
+    }
+    else
+    {
+        notesContainer.innerHTML = '';
+    }
 }
 
 main()
